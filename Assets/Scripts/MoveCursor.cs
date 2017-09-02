d879d53796b8ff6308084f0bb0baff641c594a16
Dominik Földi <Dominik.Foldi@outlook.com>
2017-09-02 14:28:59 +0000

    Added script to move cursor object

---
+++ Assets/Scripts/MoveCursor.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class MoveCursor : MonoBehaviour {
+
+    void FixedUpdate()
+    {
+        RaycastHit hit;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out hit))
+        {
+            transform.position = new Vector3(hit.point.x, 0, hit.point.z);
+        }
+    }
+}
