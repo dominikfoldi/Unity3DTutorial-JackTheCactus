f13146fb6ac463b8d50946bc1c95000e00b1a754
Dominik Földi <dominik.foldi@outlook.com>
2017-09-04 23:07:10 +0200

    Squash from dev
    * Created project
    
    * Created Scene
    
    * Imported cactus character
    
    * Placed cactus and positioned camera
    
    * Added Ground object
    
    * Added Cursor object
    
    * Added script to move cursor object
    
    * Added character movement script and animations

---
+++ Assets/Scripts/FollowCursor.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class FollowCursor : MonoBehaviour {
+
+    private Transform _cursorTransform;
+    private Animator _animator;
+
+    public float speed = 100f;
+
+	void Awake ()
+    {
+        _cursorTransform = GameObject.FindGameObjectWithTag("Cursor").transform;
+        _animator = GetComponent<Animator>();
+    }
+	
+	void Update ()
+    {
+        float step = speed * Time.deltaTime;
+        transform.position = Vector3.MoveTowards(transform.position, _cursorTransform.position, step);
+        transform.rotation = Quaternion.FromToRotation(Vector3.forward, _cursorTransform.position);
+
+        bool isWalking = transform.position != _cursorTransform.position;
+        _animator.SetBool("IsWalking", isWalking);
+	}
+}
